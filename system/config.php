c288cdb645f48a01a20076460b98e1d52f3c96c7
marcelojsil <dev.marcelojs@gmail.com>
2022-12-16 17:47:57 -0300

    initial

---
+++ system/config.php
@@ -0,0 +1,16 @@
+<?php 
+
+$nome_empresa = "Empresa de Teste";
+$url = "http://localhost/gestao/";
+$endereco_empresa = "Av. Padre José, 150, Alto do Cardoso";
+$fone_empresa = "(12) 3642-0000";
+$email_admin = "empresa@empresa.com";
+$rodape_relatorios = "Desenvolvido por Marcelo Silva - Tech Marthe";
+
+//VARIAVEIS DO BANCO DE DADOS LOCAL
+$servidor = 'localhost';
+$usuario = 'root';
+$senha = '';
+$banco = 'gestao';
+
+?>
